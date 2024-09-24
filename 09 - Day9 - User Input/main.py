6da65c860d6b4f3ce5282732101518a5d8a98492
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-09-24 20:07:41 +0530

    Taking user input with type casting

---
+++ 09 - Day9 - User Input/main.py
@@ -0,0 +1,9 @@
+a = input("Enter your name:\t")
+print(a)
+
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print(x+y)
