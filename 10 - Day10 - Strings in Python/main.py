5dc0f24a734bfbcc8ef80547d6445bdd9e93b4f0
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-09-24 20:43:49 +0530

    Learnt about Strings in Python

---
+++ 10 - Day10 - Strings in Python/main.py
@@ -0,0 +1,19 @@
+name = "Raunak"
+friend = "Rohan"
+friend2 = "Harry"
+
+print("Hello,",name)
+
+apple = "He said, \"An apple a day keeps a doctor away\""
+print(apple)
+
+orange = """Hi my name is Raunak Mishra
+I am currently pursuing my Btech Degree from IEM
+I am in 2nd yr"""
+
+print(orange)
+print(name[0])
+
+
+for character in orange:
+    print(character)
