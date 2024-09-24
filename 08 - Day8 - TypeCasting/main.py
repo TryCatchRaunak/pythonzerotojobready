007861ee27e4e20f451b111390b58e51d2550aa0
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2024-09-24 19:54:19 +0530

    Learnt about type casting in python

---
+++ 08 - Day8 - TypeCasting/main.py
@@ -0,0 +1,17 @@
+a = "1"
+b = "2"
+
+# For this type of operation we get an output of 12 because python takes a and b as strings and thus concatenated them
+
+
+# a = 1
+# b = 2
+print(int(a) + int(b))
+
+
+#Implicit type casting
+
+c = 1.9
+d = 8
+print(c+d)
+print(type(c))
