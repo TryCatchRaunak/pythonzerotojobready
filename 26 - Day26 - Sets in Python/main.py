6d60fd2931b61e8c428b8889a21d4f44ceb2a9fe
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2025-03-29 21:21:33 +0530

    Learnt about sets in Python

---
+++ 26 - Day26 - Sets in Python/main.py
@@ -0,0 +1,12 @@
+s = {2,4,6,4}#In a set values are not repeated and order is not maintained
+print(s)
+
+
+info = {"Raunak", 4.6, 32, False, 32}#Can store all types of data-types
+print(info)
+
+raunak = set()#creates empty set
+print(type(raunak))
+
+for value in info:
+    print(value)
