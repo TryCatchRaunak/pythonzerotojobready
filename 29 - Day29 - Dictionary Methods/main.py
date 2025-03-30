7820d32dad33b085ad3a3f04228b4197b3ab9458
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2025-03-30 09:09:30 +0530

    Performed Dictionary methods

---
+++ 29 - Day29 - Dictionary Methods/main.py
@@ -0,0 +1,11 @@
+ep1 = {122: 43, 3234:43, 93: 64, 738:92}
+ep2 = {222:67, 5476:87}
+
+ep1.update(ep2)
+print(ep1)
+
+#Similarly we can use other methods:-
+# 1. clear()
+# 2. pop()
+# 3. del()
+# and more to get in Python documentation
