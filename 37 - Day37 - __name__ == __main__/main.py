8bd9161a52d9df1cf4c91d586999478552442463
Raunak Mishra <69429073+TryCatchRaunak@users.noreply.github.com>
2025-05-12 14:13:09 +0530

    __name__ == "__main__"

---
+++ 37 - Day37 - __name__ == __main__/main.py
@@ -0,0 +1,3 @@
+import raunak
+
+raunak.welcome()
---
+++ 37 - Day37 - __name__ == __main__/raunak.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("You're welcome on behalf of Raunak")
+
+print(__name__)
+if __name__ == "__main__":
+#Had not it been used the welcome() function would have executed twice 
+    welcome()
